fe1f85541b5844ffad31c8183d14cbef8d96275c
Valerie Tremblay <valerie@cliento.ca>
2023-02-16 19:25:24 -0500

    Mapping template

--- functions/async.ts
+++ functions/async.ts
@@ -1,7 +1,8 @@
-import {APIGatewayProxyEvent} from 'aws-lambda';
 
 
-export const handler = async (event: APIGatewayProxyEvent) => {
+export const handler = async (event: any, context: any) => {
+    console.log("Event:" + JSON.stringify(event));
+    console.log("Other:" + JSON.stringify(context));
     console.log("Hello");
     function wait(){
         return new Promise((resolve, reject) => {
